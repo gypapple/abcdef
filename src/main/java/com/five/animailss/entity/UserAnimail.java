8c1656956e886bbabd61ad0f8359b8bb2ed63e8d
gypapple <ygao8347@gmail.com>
2019-01-11 17:07:35 +0800

    萌宠数据接口

---
+++ src/main/java/com/five/animailss/entity/UserAnimail.java
@@ -0,0 +1,43 @@
+package com.five.animailss.entity;
+
+public class UserAnimail {
+    private Integer id;
+
+    private Integer uId;
+
+    private Integer aId;
+
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getuId() {
+        return uId;
+    }
+
+    public void setuId(Integer uId) {
+        this.uId = uId;
+    }
+
+    public Integer getaId() {
+        return aId;
+    }
+
+    public void setaId(Integer aId) {
+        this.aId = aId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+}
